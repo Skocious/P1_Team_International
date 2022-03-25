96419c27cc9cf21b7a9084624957a68ef926abe2
William Blair <blaircorporate@gmail.com>
2022-03-25 10:47:51 -0500

    Changed reimbursement entity to request_id

---
+++ william/tests/employee_tests/test_employee_dao.py
@@ -0,0 +1,12 @@
+from entities.employee import Employee
+from william.implementation.employee_imp import EmployeeImp
+
+employee_dao = EmployeeImp()
+
+
+def test_get_employee_by_employee_id():
+    test_employee = Employee(10, "Barry", "White")
+    returned_employee = employee_dao.get_employee_by_id(test_employee)
+    assert returned_employee.employee_id == 10
+    print(returned_employee)
+
