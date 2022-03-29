a970c1658911325e3024500512a7e0f925fc9176
Jeryl Skinner <skinner.jeryl@gmail.com>
2022-03-29 18:31:04 -0400

    Acceptance Criteria

--- entities/reimbursement.py
+++ entities/reimbursement.py
@@ -1,8 +1,8 @@
 class Reimbursement:
-    def __init__(self, request_id, request_type, balance, comment, employee_id, status):
-        self.request_id = request_id
+    def __init__(self, request_type, balance, comment):
+        self.request_id = None
         self.request_type = request_type
         self.balance = balance
         self.comment = comment
-        self.employee_id = employee_id
-        self.status = status
+        self.employee_id = None
+        self.status = None
