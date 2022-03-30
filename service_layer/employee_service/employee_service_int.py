75e4c781a1c4e2e48c89d8675fe2cd2068159341
William Blair <blaircorporate@gmail.com>
2022-03-30 16:43:29 -0500

    added eployee service layer and custom exceptions

--- service_layer/employee_service/employee_service_int.py
+++ service_layer/employee_service/employee_service_int.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 
+from data_access_layer.employee_dal.employee_dao import EmployeeDaoImp
 from entities.employee import Employee
 
 
-class EmployeeDaoInterface(ABC):
+class EmployeeServiceInt(ABC):
+
+    def __init__(self, employee_dao: EmployeeDaoImp):
+        self.employee_dao: EmployeeDaoImp = employee_dao
 
     @abstractmethod
     def read_employee_info(self, employee: Employee) -> Employee:
@@ -12,6 +16,3 @@ class EmployeeDaoInterface(ABC):
     @abstractmethod
     def update_employee(self, employee: Employee) -> Employee:
         pass
-
-
-
